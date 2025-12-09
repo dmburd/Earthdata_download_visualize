681c1b6c75213d9384d54b8b3d9abc0a0615f139
Dmitry Burdeiny <dmitry.burdeiny@gmail.com>
2025-12-09 22:15:42 +0000

    create_partial_mirror minimal working example

--- create_partial_mirror/main.py
+++ create_partial_mirror/main.py
@@ -1,14 +1,11 @@
 import os
 import pprint
-import warnings
 from pathlib import Path
 
 import earthaccess
 import hydra
-import numpy as np
 import xarray as xr
 from omegaconf import DictConfig
-from pydap.client import open_url
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -48,8 +45,11 @@ def find_opendap_url(granule_meta: dict) -> str | None:
 
 @hydra.main(config_path='.', config_name='config', version_base=None)
 def main(cfg: DictConfig) -> None:
-    if 'EARTHDATA_TOKEN' not in os.environ:
-        os.environ['EARTHDATA_TOKEN'] = cfg.earthdata_access.edl_token
+    if 'EARTHDATA_USERNAME' not in os.environ:
+        os.environ['EARTHDATA_USERNAME'] = cfg.earthdata_access.username
+
+    if 'EARTHDATA_PASSWORD' not in os.environ:
+        os.environ['EARTHDATA_PASSWORD'] = cfg.earthdata_access.password
 
     # initialize an authenticated session
     session = earthaccess.login(strategy='environment')   # uses EARTHDATA_TOKEN
@@ -69,87 +69,31 @@ def main(cfg: DictConfig) -> None:
         if not opendap_base:
             print(" No OPeNDAP URL found in RelatedUrls for this granule; skipping.")
             continue
-        #print(" OPeNDAP URL (base):", opendap_base)
+
         subset_url_dap4 = opendap_base.replace("https://", "dap4://")
-        #print("subset_url_dap4 (trying):", subset_url_dap4)
 
-        selected_np_arrays = {}
-
-        # use pydap.open_url()
-        if False:
-            try:
-                ds_pydap = open_url(subset_url_dap4, session=session.get_session())
-                #print(" Successfully called `open_url(subset_url_dap4, session=session.get_session())`.")
-                #print(" Variables:", list(ds_pydap.keys())[:10], "...")
-            except Exception as e:
-                warnings.warn(f"xarray/pydap failed to open subset_url: {e}")
-                continue
-
-            for var_name_slashes in cfg.observable_vars:
-                var_name_underscores = var_name_slashes[1:].replace('/', '_')
-                arr_obj = ds_pydap[var_name_underscores]
-                data_fetched = arr_obj[:]
-                
-                if hasattr(data_fetched, "array"):
-                    np_data = np.asarray(data_fetched.array)
-                elif hasattr(data_fetched, "data"):
-                    np_data = np.asarray(data_fetched.data)
-                else:
-                    np_data = np.asarray(data_fetched)
-
-                selected_np_arrays[var_name_slashes] = np_data
+        ds_xr = xr.open_dataset(
+            subset_url_dap4,
+            engine='pydap',
+            session=session.get_session(),
+            decode_timedelta=True,
+        )
         
-        # use xarray.open_dataset()
-        # and run a loop over `cfg.observable_vars`
-        if False:
-            ds_xr = xr.open_dataset(
-                opendap_base,
-                decode_timedelta=True,
-            )
-            
-            to_save = xr.Dataset()
+        to_save = xr.Dataset()
+        var_name_underscores = [
+            var_name_slashes[1:].replace('/', '_')
+            for var_name_slashes in cfg.observable_vars
+        ]
+        to_save = ds_xr[var_name_underscores]
 
-            for var_name_slashes in cfg.observable_vars:
-                var_name_underscores = var_name_slashes[1:].replace('/', '_')
-                #np_data = ds_xr[var_name_underscores].values
-                #selected_np_arrays[var_name_slashes] = np_data
-                to_save[var_name_underscores] = ds_xr[var_name_underscores]
-                transformed_granume_name = title.split(':')[1]
-
-            outfile = Path(cfg.partial_mirror.rootdir) / f"{transformed_granume_name}.nc"
-            to_save.to_netcdf(outfile, format="NETCDF4")
-            # Quick check:
-            # print()
-            # with xr.open_dataset(outfile, engine="netcdf4") as ds:
-            #     print({n: (ds[n].dtype, ds[n].shape) for n in ds.data_vars})
-
-        # use xarray.open_dataset()
-        # and don't run a loop over `cfg.observable_vars`
-        if True:
-            transformed_granume_name = title.split(':')[1]
-            
-            ds_xr = xr.open_dataset(
-                opendap_base,
-                decode_timedelta=True,
-            )
-            
-            to_save = xr.Dataset()
-            var_name_underscores = [
-                var_name_slashes[1:].replace('/', '_')
-                for var_name_slashes in cfg.observable_vars
-            ]
-            to_save = ds_xr[var_name_underscores]
-            outfile = Path(cfg.partial_mirror.rootdir) / f"{transformed_granume_name}.nc"
-            to_save.to_netcdf(outfile, format="NETCDF4")
-
-        breakpoint()
+        transformed_granume_name = title.split(':')[1]
+        outfile = Path(cfg.partial_mirror.rootdir) / f"{transformed_granume_name}.nc"
+        to_save.to_netcdf(outfile, format="NETCDF4")
 
 
 if __name__ == "__main__":
     main()
 
 
+## cd create_partial_mirror
 ## python main.py
-## python main.py train.batch_size=128 dataset.name=cifar10 debug=true
-## ^ or group.key=value
-##
